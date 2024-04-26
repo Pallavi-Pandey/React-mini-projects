72c2282ea827f270423e491cb868c57083fd2f87
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-04-26 18:10:32 +0530

    5:57:14

---
+++ food-recipe-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <div>
+      <div className='min-h-screen p-6 bg-white text-gray-600 text-lg'>
+        <Navbar />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
