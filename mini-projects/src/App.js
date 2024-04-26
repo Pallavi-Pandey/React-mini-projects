c27fa8e1ba5f2c71fe110c4064cd395509a334d2
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-04-26 16:34:14 +0530

    Custom-Model-Popup completed

--- mini-projects/src/App.js
+++ mini-projects/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Accordian from './components/accordian';
+import ModalTest from "./components/custom-modal-popup/modal-test";
+
 
 function App() {
   return (
     <div className="App">
       {/* Accordian component */}
-      <Accordian/>
+      {/* <Accordian/> */}
 
+      {/* Custom Modal Component */}
+      <ModalTest/>
 
     </div>
   );
